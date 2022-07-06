cf89f570af910dab842d8c1488588b7749dd6056
Devreese Jorik <jorik.devreese@student.howest.be>
2022-07-06 12:26:50 +0200

    added dynamic loading of objects

--- src/components/ObjectLoader.jsx
+++ src/components/ObjectLoader.jsx
@@ -1,3 +1,6 @@
+import React, { Component,useEffect, useState} from 'react';
+import { BoxBufferGeometry } from 'three';
+
 export async function getJsonByProjectId(id) {
     // return ["hello"]
     return fetch(`https://circl.be/nieuw/tool/model.php?project=${id}&json`)
@@ -10,13 +13,47 @@ export async function getJsonByProjectId(id) {
     });
 }
 
-export function LoadObjectsFromJson() {}
+export async function loadObjectsFromJson(projectId) {
+    const modelData = await getJsonByProjectId(projectId)
+    const latestModelData = modelData[modelData.length-1]
+    console.log(latestModelData)
+    
+    let objects = []
+    let object
+    let i = 0
+    for (let obj of latestModelData.elements) {
+        i += 1
+        object = loadObj(obj,i)
+        if (object) { objects.push(object)}
+    }
+    return objects;
+    //  id
+    //  name
+    //  parcel
+    //  volumes
+    //  elements
+    //  performance
+
+}
 
 // Note: write dynamically so any obj version can be positioned 
-export function LoadObjAtPosition() {}
+export function loadObj(obj,iter) {
+    if (obj.type == "wall") {
+        return Cuboid(iter,obj.type,[obj.width/1000,obj.height/1000,obj.depth/1000],[obj.posX/1000,obj.posZ/1000,obj.posY/1000],obj.fill)
+    }
+}
 
 // NOTE: if obj is not found, load based on points => LoadCuboid
-export function LoadObjectById() {}
+export function loadObjectById() {}
 
 // NOTE: This will create custom cuboid, not from a .obj file
-export function LoadCuboid() {}
+
+const Cuboid = (iter,type,shape,pos,fill) => {
+    return(
+      <mesh position={pos} key={`mesh${type}${iter}`}>
+        <boxBufferGeometry attach="geometry" args={shape}/>
+        <meshBasicMaterial attach="material" color={fill}/>
+        <axesHelper args={[5]}/>
+      </mesh>
+    )
+}
