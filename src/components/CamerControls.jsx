40dc7cbeba8ba96a654d8ab8414e729388f81b9f
Emmeline Martens <xemmelinex.martens@gmail.com>
2022-07-07 13:41:33 +0200

    CameraFiles

---
+++ src/components/CamerControls.jsx
@@ -0,0 +1,9 @@
+import {useFrame} from '@react-three/fiber'
+
+const CameraControls = ({}) => {
+    return (
+
+    )
+}
+
+export default CameraControls;
---
+++ src/components/CamerState.js
@@ -0,0 +1,5 @@
+const state = {
+    activeMesh:null
+}
+
+export default state;
