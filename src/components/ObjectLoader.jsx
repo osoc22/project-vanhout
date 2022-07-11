56ba28bc687b2a7bfcf649c08c9dfb758bbed446
Devreese Jorik <jorik.devreese@student.howest.be>
2022-07-11 13:17:47 +0200

    corners working

--- src/components/ObjectLoader.jsx
+++ src/components/ObjectLoader.jsx
@@ -1,4 +1,7 @@
-import React, { Component,useEffect, useState} from 'react';
+import React, { Component,useEffect, useState, useMemo} from 'react';
+import { BufferGeometry, BufferAttribute, MeshBasicMaterial,DoubleSide} from 'three';
+
+let wallcolor = "#777777";
 
 export async function getJsonFromUrl(url) {
     return fetch(url)
@@ -40,19 +43,16 @@ export async function loadObjectsFromJson(projectId) {
     const latestModelData = modelData[modelData.length-1]
     // console.log(latestModelData)
     
-    let objects = []
-    let object
-    let i = 0
+    let objects = [];
+    let object;
+    let obj;
 
-    // return [
-    //     TestCuboid(1,"test",[5,90,10],[5,10,0]),
-    //     TestCuboid(1,"test",[5,90,10],[5,10,40])
-    
-    // ] // [width, height, depth], [x,y,z]
+    //objects.push(LoadParcel(latestModelData.parcel));
 
-    for (let obj of latestModelData.elements) {
-        i += 1
-        object = loadObj(obj,i)
+    for (let i=0;i<latestModelData.elements.length;i++) {
+        obj = latestModelData.elements[i]
+        object = loadObj(obj,i);
+
         if (object) { objects.push(object)}
     }
     return objects;
@@ -60,33 +60,103 @@ export async function loadObjectsFromJson(projectId) {
 
 // Note: write dynamically so any obj version can be positioned 
 export function loadObj(obj,iter) {
-    if (obj.type != "building" && obj.fill != "none") {
-        if (obj.type == "wall" && obj.properties["wall-type"] == "open") {obj.fill = "#885511"}
-        return Cuboid(iter,obj.type,[obj.width/1000,obj.height/1000,obj.depth/1000],[obj.posX/1000,obj.posZ/1000,obj.posY/1000],obj.fill,obj.theta)
+    if (["building"].indexOf(obj.type) == -1) {
+        if (obj.fill === 'none') {return}
+        if (obj.type == "corner") { return loadCorner(obj.points,obj.height)}
+        if (obj.type == "wall" && obj.properties["wall-type"] == "open") {obj.fill = wallcolor}
+        return Cuboid(iter,obj.type,[obj.width/1000,obj.height/1000,obj.depth/1000],[-obj.posX/1000,obj.posZ/1000,obj.posY/1000],obj.fill,obj.theta)
     }
 }
 
 // NOTE: if obj is not found, load based on points => LoadCuboid
 export function loadObjectById() {}
 
+
+export function loadCorner(points,height){
+    let vertices = []
+    let normalizedPoints = []
+
+    for (let i = 0;i < points.length-2; i++) {
+        normalizedPoints.push(points[0])
+        normalizedPoints.push(points[i])
+        normalizedPoints.push(points[i+1])
+
+        normalizedPoints.push([points[0][0],points[0][1],points[0][2]+height])
+        normalizedPoints.push([points[i][0],points[i][1],points[i][2]+height])
+        normalizedPoints.push([points[i+1][0],points[i+1][1],points[i+1][2]+height])
+
+        normalizedPoints.push([points[i][0],points[i][1],points[i][2]])
+        normalizedPoints.push([points[i][0],points[i][1],points[i][2]+height])
+        normalizedPoints.push([points[i+1][0],points[i+1][1],points[i+1][2]])
+
+        normalizedPoints.push([points[i][0],points[i][1],points[i][2]+height])
+        normalizedPoints.push([points[i+1][0],points[i+1][1],points[i+1][2]])
+        normalizedPoints.push([points[i+1][0],points[i+1][1],points[i+1][2]+height])
+    }
+
+    for (let i = 0; i< normalizedPoints.length;i++) {
+        let polygon = normalizedPoints[i]
+        vertices.push([-polygon[0]/1000,polygon[2]/1000,polygon[1]/1000])
+    }
+   
+    const positions = [];
+    for (const vertex of vertices) {
+        positions.push(...vertex);
+    }
+
+    const geometry = new BufferGeometry();
+    const positionNumComponents = 3;
+    geometry.setAttribute(
+        'position',
+        new BufferAttribute(new Float32Array(positions), positionNumComponents));
+    
+    return <mesh geometry={geometry}><meshBasicMaterial attach="material" side={DoubleSide} color={wallcolor}/></mesh>
+}
+
+const Box = () => {
+    return(
+      <mesh>
+      <boxGeometry/>
+      <meshBasicMaterial/>
+      {/* Check the Axes of the object, args is the size*/}
+      <axesHelper args={[5]}/>
+      </mesh>
+    )
+  }
+  
+
+export function LoadParcel(points) {
+    let vertices = []
+    for (let polygon of points) {
+        vertices.push([polygon[0]/1000,0,polygon[1]/1000])
+    }
+   
+    const positions = [];
+    for (const vertex of vertices) {
+        positions.push(...vertex);
+    }
+
+    const geometry = new BufferGeometry();
+    const positionNumComponents = 3;
+    geometry.setAttribute(
+        'position',
+        new BufferAttribute(new Float32Array(positions), positionNumComponents));
+    
+    return <mesh geometry={geometry}></mesh>
+}
+
 // NOTE: This will create custom cuboid, not from a .obj file
 
 const Cuboid = (iter,type,shape,pos,fill,theta) => {
-    //  rotation={[0,theta * 180 / Math.PI, 0]}
-    //let pivot = <boxBufferGeometry position={[pos[0]-shape[0]/2,pos[1]-shape[1]/2,pos[2]-shape[2]/2]}/>
-
-    // const ref = useUpdate(group => {
-    //     group.rotateX(theta * 180 / Math.PI)
-    // }, [])
-
-    return(
-        <group position={pos} rotation={[0,-theta,0]}>
-            <mesh key={`mesh${type}${iter}${fill}`} position={[shape[0]/2,shape[1]/2,shape[2]/2]}>
+     return(
+        <group key={`pivot${type}${iter}${fill}`} position={pos} rotation={[0,theta,0]}>
+            <mesh key={`mesh${type}${iter}${fill}`} position={[-shape[0]/2,shape[1]/2,shape[2]/2]}>
                 <boxBufferGeometry attach="geometry" args={shape}/>
                 <meshBasicMaterial attach="material" color={fill}/>
                 {/* <axesHelper args={[5]}/> */}
             </mesh>
-            {/* <axesHelper args={[5]}/> */}
+            <axesHelper args={[5]}/>
         </group>
     )
 }
+
